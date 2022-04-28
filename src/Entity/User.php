5723e0cb0e38b724366995f09504bc5d7440c010
nosfair <nosfairalchtion@gmail.com>
2022-04-28 15:06:53 +0200

    create entities and database

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,11 +2,247 @@
 
 namespace App\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
+
 /**
  * Class User
+ * @ORM\Entity
+ * @UniqueEntity(fields={"email"}, message="Il existe déjà un compte avec cette adresse mail")
+ * @UniqueEntity(fields={"userName"}, message="Il existe déjà un membre avec ce pseudonyme")
  */
 
-class user
+class User
 {
+    /**
+     * @var int
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private int $id;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=50, unique=true)
+     * @Assert\NotBlank(message="username property can not be empty")
+     * @Assert\Length(min=3, minMessage="username property must contain at least {{ limit }} characters")
+     */
+    private string $userName;
+
+    /**
+     * @var array
+     * @ORM\Column(type="json")
+     */
+    private array $roles;
+
+    /**
+     * @var string The hashed password
+     * @ORM\Column(type="string")
+     */
+    private string $password;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message="You must provide a first name")
+     */
+    private string $firstName;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message="You must provide a last name")
+     */
+    private string $lastName;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=180, unique=true)
+     * @Assert\Email(message="The email you provide is not a valid email address")
+     */
+    private string $email;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Customer::class, inversedBy="users")
+     * @ORM\JoinColumn(nullable=false)
+     * @Assert\NotBlank(message="A user has to be related to a customer")
+     */
+    private $customer;
     
+
+    /**
+     * Get the value of email
+     *
+     * @return  string
+     */ 
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set the value of email
+     *
+     * @param  string  $email
+     *
+     * @return  self
+     */ 
+    public function setEmail(string $email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of lastName
+     *
+     * @return  string
+     */ 
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * Set the value of lastName
+     *
+     * @param  string  $lastName
+     *
+     * @return  self
+     */ 
+    public function setLastName(string $lastName)
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of firstName
+     *
+     * @return  string
+     */ 
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * Set the value of firstName
+     *
+     * @param  string  $firstName
+     *
+     * @return  self
+     */ 
+    public function setFirstName(string $firstName)
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of password
+     *
+     * @return  string
+     */ 
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set the value of password
+     *
+     * @param  string  $password
+     *
+     * @return  self
+     */ 
+    public function setPassword(string $password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of roles
+     *
+     * @return  array
+     */ 
+    public function getRoles()
+    {
+        return $this->roles;
+    }
+
+    /**
+     * Set the value of roles
+     *
+     * @param  array  $roles
+     *
+     * @return  self
+     */ 
+    public function setRoles(array $roles)
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of userName
+     *
+     * @return  string
+     */ 
+    public function getUserName()
+    {
+        return $this->userName;
+    }
+
+    /**
+     * Set the value of userName
+     *
+     * @param  string  $userName
+     *
+     * @return  self
+     */ 
+    public function setUserName(string $userName)
+    {
+        $this->userName = $userName;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of id
+     *
+     * @return  int
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get the value of customer
+     */ 
+    public function getCustomer()
+    {
+        return $this->customer;
+    }
+
+    /**
+     * Set the value of customer
+     *
+     * @return  self
+     */ 
+    public function setCustomer($customer)
+    {
+        $this->customer = $customer;
+
+        return $this;
+    }
 }
